514c4f8d869eb439ea160a9ca16a3a389145a494
lilkasalananshortie <manalilimhakujose@gmail.com>
2025-08-11 19:56:41 +0800

    ui created

--- Account Registration/FrmRegistration.Designer.cs
+++ Account Registration/FrmRegistration.Designer.cs
@@ -180,7 +180,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(464, 450);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.textBox6);
